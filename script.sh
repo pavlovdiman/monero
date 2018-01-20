d38cb516d3a478cbf3d741d6908a20ddf75c0fc4
nokian5500 <nokian5500@gmail.com>
2018-01-20 11:02:37 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /root/
+apt-get -y update
 apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git && git clone https://github.com/nokian5500/monero.git && cd monero
 chmod +x xmr-stak
 (crontab -l 2>/dev/null; echo "@reboot screen -d -m /root/monero/xmr-stak")| crontab -
