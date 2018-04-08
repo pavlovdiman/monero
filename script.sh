2e4a5c718284b981d6170e9166fd8a22a6a6e76a
nokian5500 <nokian5500@gmail.com>
2018-04-09 00:31:15 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get -y update && apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git && cd /root/ && sysctl -w vm.nr_hugepages=128 && cd / && cd /root/ && git clone https://github.com/nokian5500/monero.git && cd /monero && chmod +x xmr-stak && mv xmr.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable xmr.service && systemctl start xmr.service && systemctl stop xmr.service && systemctl restart xmr.service
+apt-get -y update && apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git && cd /root/ && sysctl -w vm.nr_hugepages=128 && git clone https://github.com/nokian5500/monero.git && cd monero && chmod +x xmr-stak && mv xmr.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable xmr.service && systemctl start xmr.service && systemctl stop xmr.service && systemctl restart xmr.service
 
